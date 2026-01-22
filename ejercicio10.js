c644a05e1d97ce16b01b79475b6d8a03320e2609
gnosis111 <111215699+gnosis111@users.noreply.github.com>
2026-01-21 22:01:17 -0600

    Add function to reverse a given string
    Implemented the 'invertirCadena' function to reverse a string.

--- ejercicio10.js
+++ ejercicio10.js
@@ -13,3 +13,22 @@ function invertirCadena(cadena) {
 module.exports = {
     invertirCadena
 };
+// Ejercicio 10: Inversión de una Cadena
+// Declara una función llamada "invertirCadena" que acepte un parámetro "cadena" y retorne la cadena invertida
+
+// Ejemplos:
+// invertirCadena("Hola Mundo") // debe retornar "odnuM aloH"
+// invertirCadena("anita lava la tina") // debe retornar "anit al aval atina"
+
+function invertirCadena(cadena) {
+    // Escribe tu código aquí
+    // 1. split("") convierte la cadena en un array de caracteres.
+    // 2. reverse() invierte el orden de los elementos en ese array.
+    // 3. join("") une los elementos del array de nuevo en una sola cadena.
+    return cadena.split("").reverse().join("");
+}
+
+// Exporta la función para que pueda ser utilizada en las pruebas
+module.exports = {
+    invertirCadena
+};
