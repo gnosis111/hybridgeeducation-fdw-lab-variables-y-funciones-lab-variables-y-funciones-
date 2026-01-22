076bc3fee60479fd74fb174a0259e190dee14443
gnosis111 <111215699+gnosis111@users.noreply.github.com>
2026-01-21 21:44:05 -0600

    Implement obtenerPI function to return PI value

--- ejercicio02.js
+++ ejercicio02.js
@@ -10,3 +10,18 @@
 module.exports = {
     obtenerPI
 };
+// Ejercicio 2: Uso de Constantes en Funciones
+// Declara una función llamada "obtenerPI" que retorne el valor 3.1416
+
+//define tu función aquí
+function obtenerPI() {
+    const PI = 3.1416; // Declaramos la constante
+    return PI;         // Retornamos el valor de la constante
+}
+
+// No modificar
+// Exporta la función para que pueda ser utilizada en otros archivos
+// Como ves, tu función debe tener el nombre obtenerPI
+module.exports = {
+    obtenerPI
+};
