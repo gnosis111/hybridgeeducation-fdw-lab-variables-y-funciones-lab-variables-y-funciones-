c926ac9f0efedbd11f8de7dc10679892455b4213
gnosis111 <111215699+gnosis111@users.noreply.github.com>
2026-01-21 21:54:58 -0600

    Implement concatenarCadenas function for string concatenation

--- ejercicio08.js
+++ ejercicio08.js
@@ -11,3 +11,15 @@
 module.exports = {
     concatenarCadenas
 };
+// Ejercicio 8: Concatenación de Cadenas
+// Declara una función llamada "concatenarCadenas" que acepte dos parámetros, "cadena1" y "cadena2", y retorne la concatenación de ambas cadenas
+
+//escribe aquí tu función. Recuerda que el nombre de la función debe ser exactamente "concatenarCadenas"
+function concatenarCadenas(cadena1, cadena2) {
+    return cadena1 + cadena2;
+}
+
+// Exporta la función para que pueda ser utilizada en las pruebas
+module.exports = {
+    concatenarCadenas
+};
