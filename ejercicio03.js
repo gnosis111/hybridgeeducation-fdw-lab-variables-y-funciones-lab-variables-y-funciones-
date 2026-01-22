a4818c69b10e034af1ea8456ec8db5e6fa3b316c
gnosis111 <111215699+gnosis111@users.noreply.github.com>
2026-01-21 21:45:51 -0600

    Implement sumar function to add two numbers

--- ejercicio03.js
+++ ejercicio03.js
@@ -8,3 +8,15 @@
 module.exports = {
     sumar
 };
+// Ejercicio 3: Suma de Dos Números
+// Declara una función llamada "sumar" que acepte dos parámetros (a y b) y retorne su suma
+
+//Escribe tu función aquí
+function sumar(a, b) {
+    return a + b;
+}
+
+// Exporta la función para que pueda ser utilizada en las pruebas
+module.exports = {
+    sumar
+};
